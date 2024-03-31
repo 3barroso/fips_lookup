5e586e45bbd91ce8ae9d1db3d48a12a0fbeffac3
Erik <erikbarroso22@gmail.com>
2024-03-31 17:11:34 -0400

    adds formatting methods to calrify the returned values

--- lib/fips_lookup.rb
+++ lib/fips_lookup.rb
@@ -70,25 +70,31 @@ module FipsLookup
     def county_lookup(state_code, county_param, return_nil)
       upcase_param = county_param.upcase
       CSV.foreach(county_file(state_code: state_code)) do |county_row|
-        if county_match_row?(county_row, upcase_param)
-          return { state_code: county_row[0], fips: (county_row[1] + county_row[2]), gnis: county_row[3],
-                   name: county_row[4], class_code: county_row[5], status: county_row[6] }
-        end
+        return formatted_county(county_row) if match_county?(county_row, upcase_param)
       end
       return_nil ? (return {}) : (raise StandardError, "No county found matching: #{county_param}" unless return_nil)
     end
 
-    def county_match_row?(row, param)
+    def match_county?(row, param)
       # row => state (AL), state fips (01), county fips (001), county gnis (00161526), name (Augtauga County), class code (H1), status (A)
       row[4].upcase == param || row[3] == param || row[2] == param || "#{row[1]}#{row[2]}" == param
+    end
+
+    def formatted_county(row)
+      {
+        state_code: row[0],
+        fips: (row[1] + row[2]),
+        gnis: row[3],
+        name: row[4],
+        class_code: row[5],
+        status: row[6]
+      }
     end
 
     def state_lookup(state_param, return_nil = false)
       upstate_param = state_param.upcase
       CSV.foreach(state_file) do |state_row|
-        if state_match_row?(state_row, upstate_param)
-          return { fips: state_row[0], code: state_row[1], name: state_row[2], ansi: state_row[3] }
-        end
+        return formatted_state(state_row) if state_match_row?(state_row, upstate_param)
       end
       return_nil ? (return {}) : (raise StandardError, "No state found matching: #{state_param}")
     end
@@ -96,6 +102,15 @@ module FipsLookup
     def state_match_row?(row, param)
       # row => state fips (01), state code (AL), state name (Alabama), ansi (01779775)
       row[0] == param || row[1] == param || row[2].upcase == param || row[3] == param
+    end
+
+    def formatted_state(row)
+      {
+        fips: row[0],
+        code: row[1],
+        name: row[2],
+        ansi: row[3]
+      }
     end
   end
 end
